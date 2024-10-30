34cac2db63da7d88cf621d11f6a5113cb080cc8d
João Pereira <joao.pereira@broadcom.com>
2024-10-30 10:50:01 -0500

    Upgrading the version of module
    Signed-off-by: João Pereira <joaopapereira@gmail.com>

--- calculator.go
+++ calculator.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/joaopapereira/versioned-module/v2/pkg/calculator"
+	"github.com/joaopapereira/versioned-module/v3/pkg/calculator"
 )
 
 func main() {
 	fmt.Println("Going to do some math")
 	calculator.AddString("1", "1")
+
+	fmt.Println("Now for some sub")
+	calculator.SubString("2", "1")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/joaopapereira/calculator-consumer
 
 go 1.23.2
 
-require github.com/joaopapereira/versioned-module/v2 v2.0.0
+require github.com/joaopapereira/versioned-module/v3 v3.0.0-20241030154732-42acd06bb6cc
