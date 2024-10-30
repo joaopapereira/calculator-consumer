3af1d5a3e2b91f938cbaa301b572109398b74a85
João Pereira <joao.pereira@broadcom.com>
2024-10-30 10:36:34 -0500

    Using v1 lib
    Signed-off-by: João Pereira <joaopapereira@gmail.com>

---
+++ calculator.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joaopapereira/versioned-module/pkg/calculator"
+)
+
+func main() {
+	fmt.Println("Going to do some math")
+	calculator.Add(1, 1)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/joaopapereira/calculator-consumer
+
+go 1.23.2
+
+require github.com/joaopapereira/versioned-module v1.0.0
